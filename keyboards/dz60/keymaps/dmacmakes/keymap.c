fc478d1926980330eb0f95101523cacd6394ee79
DannyMcGillick <danny.mcgillick@gmail.com>
2018-07-30 12:42:36 +1000

    Moved lighting buttons over one spot

--- keyboards/dz60/keymaps/dmacmakes/keymap.c
+++ keyboards/dz60/keymaps/dmacmakes/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,   KC_F1,  KC_F2,    KC_F3,  KC_F4,    KC_F5,      KC_F6,  KC_F7,    KC_F8,   KC_F9,   KC_F10,   KC_VOLD, KC_VOLU,   KC_SLEP, KC_DEL,
       ______,   ______, ______,   KC_UP,  ______,   ______,    ______, ______,    KC_UP,  ______,   ______,   ______,  ______,    RESET,
       KC_CAPS, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, ______,   KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_END,   ______,   ______,
-      ______, ______, ______, ______, BL_DEC, BL_TOGG, BL_INC, ______, ______, ______, ______, ______, KC_PGUP, ______,
+      ______, ______, ______, ______, ______, BL_DEC, BL_TOGG, BL_INC, ______, ______, ______, ______, KC_PGUP, ______,
       ______, ______, ______, KC_ENTER, ______, ______, ______, ______, KC_HOME, KC_PGDN, KC_END
       ),
 };
