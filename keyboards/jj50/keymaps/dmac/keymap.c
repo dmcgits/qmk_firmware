dbfd2c26807d860db39310917c4bd737e8d273b1
DMacMakes <danny.mcgillick@gmail.com>
2019-03-10 21:02:24 +1100

    JJ50 new brackety places

--- keyboards/jj50/keymaps/dmac/keymap.c
+++ keyboards/jj50/keymaps/dmac/keymap.c
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [_DEFLT] = LAYOUT( \
-        KC_GRAVE,           KC_1,       KC_2,    KC_3,    KC_4,    KC_5,        KC_6,       KC_7,       KC_8,       KC_9,       KC_0,                      KC_BSPACE,  \
-        KC_TAB,             KC_Q,       KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,                      KC_PIPE,    \
-        LT(_LOWER, KC_ESC), KC_A,       KC_S,    KC_D,    KC_F,    KC_G,        KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,                   KC_QUOT,    \
-        KC_LSFT,            KC_ENTER,   KC_Z,    KC_X,    KC_C,    KC_V,        KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,                    MT(MOD_RSFT, KC_SLASH),     \
-        KC_LCTL,            KC_LCTL,    KC_LGUI, KC_LALT, KC_SPACE,MO(_RAISE),  MO(_RAISE),KC_SPC,     KC_ENTER,    KC_MINUS,   KC_EQUAL,                  KC_DEL      \
+        KC_GRAVE,           KC_1,       KC_2,    KC_3,    KC_4,    KC_5,        KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPACE,  \
+        KC_TAB,             KC_Q,       KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,    \
+        LT(_LOWER, KC_ESC), KC_A,       KC_S,    KC_D,    KC_F,    KC_G,        KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    \
+        KC_LSFT,            KC_ENTER,   KC_Z,    KC_X,    KC_C,    KC_V,        KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     MT(MOD_RSFT, KC_SLASH),     \
+        KC_LCTL,            KC_LCTL,    KC_LGUI, KC_LALT, KC_SPACE,MO(_RAISE),  MO(_RAISE),KC_SPC,     KC_ENTER,    KC_MINUS,   KC_EQUAL,   KC_DEL      \
     ),
 
     /* Raise
@@ -85,11 +85,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_RAISE] = LAYOUT( \
-       KC_F12,   KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_MINUS,   KC_EQUAL,  KC_F11,  \
-       KC_ENTER, KC_SYSTEM_SLEEP,  KC_PGDN, KC_UP,   KC_PGUP, KC_5,    KC_6,    KC_PGDN, KC_UP,   KC_PGUP, KC_PSCREEN,    RESET,  \
-       KC_CAPSLOCK,  KC_HOME,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_ENT,  \
-       _______, KC_ENTER,KC_BSPACE, KC_DEL,  RGB_TOG,  RGB_VAD,  RGB_VAI, RGB_HUD, KC_MPLY, KC_VOLD, KC_VOLU, KC_QUES,  \
-       _______, _______, _______, _______, _______, _______, _______, _______, ______, KC_VOLD, KC_VOLU, _______  \
+       KC_F12,                KC_F1,          KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_MINUS,   KC_EQUAL,  KC_F11,  \
+       KC_ENTER,    KC_SYSTEM_SLEEP,        KC_PGDN, KC_UP,   KC_PGUP, KC_5,    KC_6,    KC_PGUP, KC_UP,   KC_PGDN, KC_PSCREEN,    RESET,  \
+       KC_CAPSLOCK,         KC_HOME,        KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_ENT,  \
+       _______,            KC_ENTER,      KC_BSPACE, KC_DEL,  RGB_TOG,  RGB_VAD,  RGB_VAI, RGB_HUD, KC_MPLY, KC_VOLD, KC_VOLU, KC_QUES,  \
+       _______,             _______,        _______, _______, _______, _______, _______, _______, ______, KC_VOLD, KC_VOLU, _______  \
     ),
 
     /* Lower
@@ -107,11 +107,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_LOWER] = LAYOUT( \
-        KC_F12, LCTL(KC_F5),      KC_F5,   KC_F10,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_MINUS,   KC_EQUAL,  KC_F11,    \
-        KC_ENTER,   _______,    _______, _______, _______,  _______, _______, _______, _______, _______, KC_PIPE, RESET,    \
-        _______,    _______, KC_LBRACKET, KC_LCBR, KC_LPRN, KC_LABK, KC_RABK,   KC_RPRN, KC_RCBR, KC_RBRACKET, _______, KC_ENT,     \
-        _______,    KC_ENTER,   _______, _______, RGB_TOG,  RGB_VAD,  RGB_VAI, RGB_HUD, RGB_HUI,RGB_SAD,RGB_SAI, _______,  \
-        BL_STEP,    _______,    _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY    \
+        KC_F12, LCTL(KC_F5),      KC_F5,     KC_F10,    KC_F4,        KC_F5,        KC_F6,      KC_F7,      KC_F8,   KC_MINUS,      KC_EQUAL,   KC_F11,    \
+        KC_ENTER,   _______,    _______,    _______,    _______,    _______,        BL_ON,      BL_OFF,     BL_DEC,      BL_INC,    KC_PSCREEN, KC_BSLS,    \
+        _______,    _______,    _______,    KC_LPRN,    KC_RPRN, KC_LBRACKET, KC_RBRACKET,      KC_LCBR,    KC_RCBR,    _______,    _______,    KC_ENT,     \
+        _______,    KC_ENTER,   _______,    RGB_MOD,    RGB_TOG,    RGB_VAD,      RGB_VAI,      RGB_HUD,    RGB_HUI,    RGB_SAD,    RGB_SAI,    RGB_MODE_PLAIN,  \
+        BL_STEP,    _______,    _______,    _______,    _______,    _______,      _______,      _______,    KC_MNXT,    KC_VOLD,    KC_VOLU,    KC_MPLY    \
     ),
     
     /* Fn
