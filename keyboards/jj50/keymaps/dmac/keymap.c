59b4bedf16bc0cb4a92b66cf07f4ad66d7cb2dea
dmac <danny.mcgillick@gmail.com>
2019-06-24 21:58:48 +1000

    swapped curlies and parentheses on er / df, tried to fix lighting

--- keyboards/jj50/keymaps/dmac/keymap.c
+++ keyboards/jj50/keymaps/dmac/keymap.c
@@ -108,8 +108,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// capsLock/esc coding function key
     [_LOWER] = LAYOUT( \
         KC_F12, LCTL(KC_F5),      KC_F5,     KC_F10,    KC_F11,  KC_F12,        KC_F6,      KC_F7,      KC_F8,   KC_MINUS,      KC_EQUAL,   KC_F11,    \
-        KC_ENTER,  KC_SYSTEM_SLEEP,    _______,     KC_LPRN,    KC_RPRN,    _______,		BL_ON,      BL_OFF,     BL_DEC,      BL_INC,    KC_PSCREEN, KC_BSLS,    \
-        _______,    _______,    _______,   KC_LCBR,    KC_RCBR, 	KC_LABK,		KC_RABK,	KC_LABK,    KC_RABK,    KC_LBRC,    KC_RBRC,    KC_ENT,     \
+        KC_ENTER,  KC_SYSTEM_SLEEP,    _______,     KC_LCBR,    KC_RCBR,    _______,		BL_ON,      BL_OFF,     BL_DEC,      BL_INC,    KC_PSCREEN, KC_BSLS,    \
+        _______,    _______,    _______,   KC_LPRN,    KC_RPRN, 	KC_LABK,		KC_RABK,	KC_LABK,    KC_RABK,    KC_LBRC,    KC_RBRC,    KC_ENT,     \
         _______,    _______,   _______,    KC_LBRC,    KC_RBRC,    RGB_VAD,		RGB_VAI,	RGB_HUD,    RGB_HUI,    RGB_SAD,    RGB_SAI,    RGB_MODE_PLAIN,  \
         BL_STEP,    _______,    _______,    _______,    _______,    _______,      _______,      _______,    KC_MNXT,    KC_VOLD,    KC_VOLU,    KC_MPLY    \
     ),
