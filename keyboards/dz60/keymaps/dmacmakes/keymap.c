3f9ccd57b6bdfe7e907d6af055635440213b74a6
DESKTOP-HOME\Danny <danny.mcgillick@gmail.com>
2019-07-12 20:38:55 +1000

    Parens, backup i key on raise [

--- keyboards/dz60/keymaps/dmacmakes/keymap.c
+++ keyboards/dz60/keymaps/dmacmakes/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   LAYOUT_directional(
-      KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_DEL, KC_BSPC,
+      KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
       LT(2, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, MT (MOD_RSFT, KC_SLASH), KC_UP, KC_DEL,
@@ -55,8 +55,8 @@ TD(TD_ENTER_SPACE)
 
   LAYOUT_directional(
       KC_ESC, LCTL(KC_F5),  KC_F5,  KC_F10,  KC_F11, KC_F12,      KC_F6,  KC_F7,    KC_F8,   KC_F9,   KC_F10,   KC_VOLD, KC_VOLU,   KC_SLEP, KC_DEL,
-      KC_ENT,      KC_SYSTEM_SLEEP, ______,   KC_LEFT_PAREN,  KC_RIGHT_PAREN,   RGB_MOD,    RGB_TOG, ______,    KC_UP,  ______,   KC_PSCR,   ______,  ______,    RESET,
-      ______, 	______, ______, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_LABK,   KC_RABK, KC_LABK,    KC_RABK,    KC_LBRC,    KC_RBRC,   ______,   KC_KP_ENTER,
+      KC_ENT,      KC_SYSTEM_SLEEP, ______,   KC_LCBR,  KC_RCBR,   RGB_MOD,    RGB_TOG, ______,    KC_UP,  ______,   KC_PSCR,   KC_I,  ______,    RESET,
+      ______, 	______, ______, KC_LPRN, KC_RPRN, KC_LABK,   KC_RABK, KC_LABK,    KC_RABK,    KC_LBRC,    KC_RBRC,   ______,   KC_KP_ENTER,
       ______, ______, KC_BSPC, KC_LBRC, KC_RBRC, BL_DEC, BL_TOGG, BL_INC, ______, ______, ______, ______, KC_VOLU, KC_INS,
       ______, ______, ______, KC_ENTER, ______, ______, ______, ______, KC_HOME, KC_VOLD, KC_END
       ),
